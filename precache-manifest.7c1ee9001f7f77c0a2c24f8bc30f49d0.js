f8cc713cec8c323b55c2d95172174b1d8136c758
liang <liang@theteam247.com>
2019-12-20 16:11:11 +0800

    Updates

---
+++ precache-manifest.7c1ee9001f7f77c0a2c24f8bc30f49d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a08f99bbbeea1a9763f32e6159939e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010fee746a4d180c841c",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "f548b562ed25579cad3d",
+    "url": "/static/css/main.b15a5ea6.chunk.css"
+  },
+  {
+    "revision": "010fee746a4d180c841c",
+    "url": "/static/js/2.313d59b5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.313d59b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bcad9deed3982b046f7",
+    "url": "/static/js/3.fd36bc5e.chunk.js"
+  },
+  {
+    "revision": "f548b562ed25579cad3d",
+    "url": "/static/js/main.736fc446.chunk.js"
+  },
+  {
+    "revision": "4eb7c74d55c899251a2d",
+    "url": "/static/js/runtime-main.fad7c0bd.js"
+  }
+]);
